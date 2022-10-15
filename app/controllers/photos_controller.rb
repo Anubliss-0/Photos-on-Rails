a074ef395d6ce11c0dedcf5449ce2a26c56a7fb1
clahflah <claytonflaherty92@gmail.com>
2022-10-15 13:31:45 +0200

    photo create and new are working, need to add an index method to fully test

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,3 +1,30 @@
 class PhotosController < ApplicationController
 
+  def new
+    @photo = Photo.new
+    @bookmark = Bookmark.new
+    authorize @photo
+  end
+
+  def create
+    @photo = Photo.new(photo_params)
+    if @photo.save!
+      params[:photo][:collection_ids].delete("")
+      params[:photo][:collection_ids].each do |collection|
+        @bookmark = Bookmark.new(photo: @photo, collection_id: collection)
+        raise
+        @bookmark.save!
+      end
+      flash[:notice] = "Photo saved."
+      redirect_to photo_path(@photo)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def photo_params
+    params.require(:photo).permit(:title, :description, :film, :camera)
+  end
 end
