79cb2fd4dc490d58c418d3c43909363a9829a100
clahflah <claytonflaherty92@gmail.com>
2022-10-20 10:48:36 +0200

    changes added for production environment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,7 +1,6 @@
 require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
-  config.action_mailer.default_url_options = { host: "http://TODO_PUT_YOUR_DOMAIN_HERE" }
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
@@ -91,4 +90,21 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+#contact form
+config.action_mailer_default_url_options = { host: 'https://your-app-site.com' }
+Rails.application.routes.default_url_options[:host] = 'https://your-app-site.com'
+config.action_mailer.delivery_method = :smtp
+config.action_mailer.perform_deliveries = true
+config.action_mailer.raise_delivery_errors = false
+config.action_mailer.default charset: 'utf-8'
+config.action_mailer.smtp_settings = {
+  address: 'smtp.gmail.com'
+  port: 587
+  domain: 'gmail.com'
+  authentication: 'plain'
+  enable_starttls_auto: true
+  user_name: ENV['GMAIL_USERNAME']
+  password: ENV['GMAIL_PASSWORD']
+}
 end
