b839c0c66719edcf319cf6181082d02396ce45a5
clahflah <claytonflaherty92@gmail.com>
2023-01-09 12:22:59 +0100

    collections page is home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: "pages#home"
+  root to: "collections#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resources :photos
   resources :collections do
