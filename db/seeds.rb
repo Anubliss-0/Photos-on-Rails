6e907b71514a6265ca04b97738a465296c508d0c
clahflah <claytonflaherty92@gmail.com>
2023-01-19 07:09:11 -0500

    seeds updated to have bigger photos

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,7 @@
     date: Faker::Date.between(from: '2014-09-23', to: '2014-09-25'),
     user_id: 1
   )
-  file = URI.open("https://picsum.photos/200/300")
+  file = URI.open("https://picsum.photos/950/550")
   photo.photo.attach(io: file, filename:"filler.jpeg", content_type:"image/jpeg")
   photo.save!
 end
@@ -16,7 +16,8 @@ end
   album = Collection.create(
     name:Faker::Artist.name,
     description: Faker::Quote.famous_last_words,
-    user_id: 1
+    user_id: 1,
+    cover: 1
   )
   album.save!
   Photo.all.each do |photo|
