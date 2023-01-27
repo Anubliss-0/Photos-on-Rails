06760f85ecc1e268ad2727e482bdb76fa6f1e04c
clahflah <claytonflaherty92@gmail.com>
2023-01-27 06:40:37 -0500

    photo info can change one time but no more need to work on increment variable

--- app/javascript/controllers/photoinfo_controller.js
+++ app/javascript/controllers/photoinfo_controller.js
@@ -3,14 +3,24 @@ import { Controller } from "@hotwired/stimulus"
 // Connects to data-controller="photoinfo"
 export default class extends Controller {
   static targets = ["photoinfo"]
+  makeTargets() {
+
+  }
   connect() {
     console.log("you did it you sexy bitch")
   }
 
   nextInfo() {
+
+    let count = 0;
+    let increment = count + 1;
+    let classNeeded = `photo${increment}`
     if(this.photoinfoTarget.classList.contains('photo-info-active') === true) {
       this.photoinfoTarget.classList.remove('photo-info-active')
       this.photoinfoTarget.classList.add('photo-info-inactive')
     }
+    this.photoinfoTargets[increment].classList.remove('photo-info-inactive')
+    this.photoinfoTargets[increment].classList.add('photo-info-active')
+    console.log(this.photoinfoTargets[increment])
   }
 }
