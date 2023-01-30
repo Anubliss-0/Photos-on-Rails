76ce1cf8410fe7ff46659766eec4315e166e00b7
clahflah <claytonflaherty92@gmail.com>
2023-01-30 07:13:13 -0500

    Step closer to getting the function working

--- app/javascript/controllers/photoinfo_controller.js
+++ app/javascript/controllers/photoinfo_controller.js
@@ -3,24 +3,27 @@ import { Controller } from "@hotwired/stimulus"
 // Connects to data-controller="photoinfo"
 export default class extends Controller {
   static targets = ["photoinfo"]
-  makeTargets() {
 
-  }
   connect() {
     console.log("you did it you sexy bitch")
   }
 
-  nextInfo() {
 
-    let count = 0;
-    let increment = count + 1;
-    let classNeeded = `photo${increment}`
+  nextInfo(event) {
+    let indexes = []
+    // let increment = count + 1;
+    this.photoinfoTargets.forEach((element, index) => { if(element.classList.contains('photo-info-active') === true) {
+      indexes.push(index)
+    } })
+    const toInt = indexes.reduce((accum, digit) => (accum * 10) + digit, 0);
+    const nextIndex = toInt + 1
+    // let number = this.photoinfoTargets.findIndex(this.photoinfoTarget.classList.contains('photo-info-active'))
     if(this.photoinfoTarget.classList.contains('photo-info-active') === true) {
       this.photoinfoTarget.classList.remove('photo-info-active')
       this.photoinfoTarget.classList.add('photo-info-inactive')
     }
-    this.photoinfoTargets[increment].classList.remove('photo-info-inactive')
-    this.photoinfoTargets[increment].classList.add('photo-info-active')
-    console.log(this.photoinfoTargets[increment])
+    this.photoinfoTargets[nextIndex].classList.remove('photo-info-inactive')
+    this.photoinfoTargets[nextIndex].classList.add('photo-info-active')
+    console.log(nextIndex)
   }
 }
