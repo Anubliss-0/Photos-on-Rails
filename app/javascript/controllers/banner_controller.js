0648ca624edd8dc682b0552d08ae32a3e1ea170b
clahflah <claytonflaherty92@gmail.com>
2023-02-10 06:39:19 -0500

    Autoscroll on homepage working

--- app/javascript/controllers/banner_controller.js
+++ app/javascript/controllers/banner_controller.js
@@ -2,6 +2,13 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="banner"
 export default class extends Controller {
+  static targets = ["collections"]
+
   connect() {
+  }
+
+  autoScroll(event) {
+    event.preventDefault()
+    this.collectionsTarget.scrollIntoView(true)
   }
 }
